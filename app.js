aa7e1e8a797f56db3cfbb28f95bd79c9c9f04c52
Baurjan <baurjanjalgaskhan@gmail.com>
2021-03-01 19:59:04 -0800

    oorchillo

--- app.js
+++ app.js
@@ -8,19 +8,28 @@ var score = [0, 0];
 // toglogchiin eeljiin tsugluulj baigaa onoog hadgalah huvisagch 
 var roundScore = 0;
 // shoo ali talaara buuhiig hadgalah huvisagch 1-6 gesen huvisagch 
-var dice = Math.floor(Math.random()*6)+1;
+
 
 // document.querySelector('#score-0').textContent = dice;
 // <div class="player-score" id="score-0">43</div>
 
 // document.querySelector('#score-0').textContent = dice;
-document.querySelector('#score-0').textContent = 0; // html ees ID gaar n haij oorchilj bna 
-document.querySelector('#score-1').textContent = 0; 
 
+document.querySelector('#score-0').textContent = "0"; // html ees ID gaar n haij oorchilj bna 
+document.getElementById("score-1").textContent="0"; // hurdan haij olno  ID
 
-document.querySelector('#current-0').textContent = 0;
-document.querySelector('#current-1').textContent = 0;// html ees class aar n haij olj oorchilno 
+document.getElementById('current-0').textContent = "0";
+document.getElementById('current-1').textContent = "0";// html ees class aar n haij olj oorchilno 
 
-document.querySelector('.dice').style.display = "none";
+var diceDom = document.querySelector('.dice');
 
-console.log("dice shoo"+ dice);
+diceDom.style.display = "none";
+document.querySelector(".btn-roll").addEventListener('click', function (){
+    var diceNumber = Math.floor(Math.random()*6)+1;
+
+    diceDom.style.display = "block";
+    diceDom.src = 'dice-'+ diceNumber + ".png";
+
+});
+
+// console.log("dice shoo"+ dice);
