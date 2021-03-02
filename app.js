141f301a1bf4c7d6d2ce6af5337abb516abf83c9
Baurjan <baurjanjalgaskhan@gmail.com>
2021-03-02 12:58:36 -0800

    oorchillo

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
 // toglogchiin eeljiig hadgalah huvisagch player1  0
-var activePlayer = 1;
+var activePlayer = 0;
 
 // toglogchiin tsugluusan onoog hadgalah huvisagch 
 var score = [0, 0];
 
 
 // toglogchiin eeljiin tsugluulj baigaa onoog hadgalah huvisagch 
+
 var roundScore = 0;
+
 // shoo ali talaara buuhiig hadgalah huvisagch 1-6 gesen huvisagch 
 
 
@@ -22,14 +24,44 @@ document.getElementById('current-0').textContent = "0";
 document.getElementById('current-1').textContent = "0";// html ees class aar n haij olj oorchilno 
 
 var diceDom = document.querySelector('.dice');
-
+// shooni zurga alga bolgono 
 diceDom.style.display = "none";
+// shoog shideh event 
 document.querySelector(".btn-roll").addEventListener('click', function (){
+
     var diceNumber = Math.floor(Math.random()*6)+1;
 
-    diceDom.style.display = "block";
-    diceDom.src = 'dice-'+ diceNumber + ".png";
+    // shoog zrg web dr gargaj irne 
+    diceDom.style.display = "block";   
+    // sanamsargvi too delgets deer gargaj irne 
+    diceDom.src = 'dice-'+ diceNumber + ".png"; 
+    
+    // buusan too 1 ees ylgaatai bol toglogch eeljiin onoog nemgdvvlne 
+    if (diceNumber !== 1){
+        roundScore = roundScore + diceNumber;
+        document.getElementById("current-"+ activePlayer).textContent = roundScore;
+    }
+
+    else {
+
+        // ene toglogchiin elljinde tsugluusan onoog 0 bolgon 
+        roundScore = 0;
+        document.getElementById("current-"+ activePlayer).textContent =0;
+        // 1 buusan toglogch eelj solinno
+        activePlayer === 0 ? (activePlayer =1) : (ctivePlayer = 0);
+        
+        // ulaan tsegiig haij oloh  
+        document.querySelector(".player-0-panel").classList.toggle("active");
+        document.querySelector(".player-1-panel").classList.toggle("active");
+
+        // shoog tvr alga bolgi 
+        diceDom.style.display = "none";
+
+        
+
+
+    }
 
 });
 
-// console.log("dice shoo"+ dice);
+//dfd
